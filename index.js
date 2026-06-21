99180c0cef828a7999d6c9b200716a48dac29f4e
Ahmed-Asid <ahmedasid249@gmail.com>
2026-06-21 12:42:55 +0600

    secured backend with jwt

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const dotenv = require('dotenv');
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
+
 dotenv.config();
 
 const uri = process.env.MONGODB_URI;
@@ -25,6 +27,26 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+const JWKS = createRemoteJWKSet(
+  new URL("http://localhost:3000/api/auth/jwks")
+)
+
+const verifyToken = async(req, res, next) => {
+  const token = req?.headers.authorization
+
+  if (!token) {
+    return res.status(401).json({ message : "Unauthorized"})
+  }
+
+  try {
+    const {payload} = await jwtVerify(token, JWKS)
+  console.log(payload)
+  next()
+  } catch {
+    return res.status(403).json({ message: "Forbidden" })
+  }
+}
 
 async function run() {
   try {
@@ -47,9 +69,13 @@ async function run() {
         res.send(result)
     })
 
-    app.patch('/api/users/:id', async(req,res) => {
+    app.patch('/api/users/:id',verifyToken, async(req,res) => {
       const id = req.params.id;
       const userData = req.body;
+
+//       console.log(id)
+// console.log(`http://localhost:8000/api/users/${id}`)
+//       console.log(userData)
 
       const result = await users.updateOne(
         {_id: new ObjectId(id)},
@@ -59,19 +85,19 @@ async function run() {
     })
 
 
-    app.post('/api/appointments', async(req, res) => {
+    app.post('/api/appointments',verifyToken, async(req, res) => {
     const newAppointment = req.body;
     const result = await appointments.insertOne(newAppointment);
     res.json(result);
     })
 
-    app.get('/api/appointments', async(req, res) => {
+    app.get('/api/appointments',verifyToken, async(req, res) => {
         const cursor = appointments.find();
         const result = await cursor.toArray();
         res.send(result);
     })
 
-    app.delete('/api/appointments/:id', async(req, res) => {
+    app.delete('/api/appointments/:id',verifyToken, async(req, res) => {
         const id = req.params.id;
         const query = {
             _id : new ObjectId(id)
@@ -80,7 +106,7 @@ async function run() {
         res.send(result)
     })
 
-    app.patch('/api/appointments/:id', async(req, res) => {
+    app.patch('/api/appointments/:id', verifyToken, async(req, res) => {
         const id = req.params.id;
         const updatedData = req.body;
 
@@ -91,7 +117,7 @@ async function run() {
         res.send(result)
     })
 
-    app.get('/api/appointments/:id', async(req, res) => {
+    app.get('/api/appointments/:id', verifyToken, async(req, res) => {
         const id = req.params.id;
         const query = { userId: id }
         const result = await appointments.find(query).toArray();
@@ -112,12 +138,10 @@ app.get('/api/doctors', (req, res) => {
     res.send(data);
 });
 
-app.get('/api/doctors/:id', (req, res) => {
+app.get('/api/doctors/:id', verifyToken,  async(req, res) => {
 
     const id = req.params.id;
-    console.log(id)
-    const doc = data.find(d => d.id === id);
-    console.log(doc)
+    const doc = await data.find(d => d.id === id);
     res.send(doc);
 });
 
